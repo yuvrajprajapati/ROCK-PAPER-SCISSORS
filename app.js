3b3ad4f9691f0419ed66c1646dcd1f71ff480526
yuvrajprajapati <130988776+yuvrajprajapati@users.noreply.github.com>
2025-02-13 05:59:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let userscore=0;
+let compscore=0;
+const choices=document.querySelectorAll(".choice");
+
+const msg=document.querySelector("#msg")
+
+const userscorepara=document.querySelector("#user-score");
+const compscorepara=document.querySelector("#comp-score");
+
+
+const gencompchoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randidx=Math.floor(Math.random()*3);
+    return options[randidx];
+}
+
+const drawgame=()=>{
+    console.log("game was draw");
+    msg.innerText="Game was draw! Play again";
+    msg.style.backgroundColor="#081b31";
+};
+
+const showwinner=(userwin,userchoice,compchoice)=>{
+    if(userwin){
+        userscore++;
+        userscorepara.innerText=userscore;
+        console.log("you win");
+        msg.innerText=`You Win your ${userchoice} beats ${compchoice}`;
+        msg.style.backgroundColor="green";
+    }
+    else{
+        compscore++;
+        compscorepara.innerText=compscore;
+        console.log("You Loose");
+        msg.innerText=`You Lost  ${compchoice} beats Your ${userchoice}`;
+        msg.style.backgroundColor="red";
+    }
+}
+
+const playgame=(userchoice)=>{
+    console.log("user choice=",userchoice);
+    const compchoice=gencompchoice();
+    console.log("comp choice",compchoice)
+
+    if(userchoice===compchoice){
+        drawgame();
+    }
+    else{
+        let userwin=true;
+        if(userchoice==="rock"){
+            userwin=compchoice==="paper"?false:true;
+        }
+        else if(userchoice==="paper"){
+                userwin=compchoice==="scissors"?false:true;
+        }
+        else{
+            compchoice==="rock"?false:true;
+        }
+        showwinner(userwin,userchoice,compchoice);
+    }
+}
+
+
+choices.forEach((choice)=>{
+    console.log(choice);
+    choice.addEventListener("click",()=>{
+        const userchoice=choice.getAttribute("id");
+        console.log("choice was clicked",userchoice);
+       playgame(userchoice);
+
+    })
+})
